6f231281b3113ad019d7ab81dda2710f09a55d50
Dillon Parker <prkr93@gmail.com>
2022-06-08 15:37:07 -0600

    Add articles state and pass down into articles component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import Articles from './Components/Articles/Articles';
 
 
 const App = () => {
+  const [articles, setArticles] = useState({});
 
-  
+  useEffect(() => {
+    getFeatured()
+    .then(data => {
+      setArticles(data)
+    })
+  }, [])
+
+
+
+
+  return (
+    <div className="App">
+      <Articles
+        articles={articles}
+      />
+    </div>
+  )
 };
 
 export default App;
