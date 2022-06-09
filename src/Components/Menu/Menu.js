46cea9dfd9a9d79889c530b0fc0a104091423212
Dillon Parker <prkr93@gmail.com>
2022-06-09 12:06:37 -0600

    Add menu items for navigation

---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,91 @@
+
+
+const Menu = () => {
+
+  return (
+    <nav>
+      <ul>
+        <li className=''>
+          <a>Home</a>
+        </li>
+        <li className=''>
+          <a>Arts</a>
+        </li>
+        <li className=''>
+          <a>Automobiles</a>
+        </li>
+        <li className=''>
+          <a>Books</a>
+        </li>
+        <li className=''>
+          <a>Business</a>
+        </li>
+        <li className=''>
+          <a>Fashion</a>
+        </li>
+        <li className=''>
+          <a>Food</a>
+        </li>
+        <li className=''>
+          <a>Health</a>
+        </li>
+        <li className=''>
+          <a>Insider</a>
+        </li>
+        <li className=''>
+          <a>Magazine</a>
+        </li>
+        <li className=''>
+          <a>Movies</a>
+        </li>
+        <li className=''>
+          <a>NY Region</a>
+        </li>
+        <li className=''>
+          <a>Obituaries</a>
+        </li>
+        <li className=''>
+          <a>Opinion</a>
+        </li>
+        <li className=''>
+          <a>Politics</a>
+        </li>
+        <li className=''>
+          <a>Real Estate</a>
+        </li>
+        <li className=''>
+          <a>Science</a>
+        </li>
+        <li className=''>
+          <a>Sports</a>
+        </li>
+        <li className=''>
+          <a>Sunday Review</a>
+        </li>
+        <li className=''>
+          <a>Technology</a>
+        </li>
+        <li className=''>
+          <a>Theater</a>
+        </li>
+        <li className=''>
+          <a>T-Magazine</a>
+        </li>
+        <li className=''>
+          <a>Travel</a>
+        </li>
+        <li className=''>
+          <a>Upshot</a>
+        </li>
+        <li className=''>
+          <a>US</a>
+        </li>
+        <li className=''>
+          <a>World</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu;
