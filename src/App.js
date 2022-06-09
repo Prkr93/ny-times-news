a90f70da5f4d701598e10a0d16c8de5b850d84b2
Dillon Parker <prkr93@gmail.com>
2022-06-09 12:07:39 -0600

    Add menu to app and refactor articles display to be dynamic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import {getFeatured} from './apiCalls';
 import {useState, useEffect} from 'react';
 import Articles from './Components/Articles/Articles';
+import Menu from './Components/Menu/Menu';
 
 
 const App = () => {
   const [articles, setArticles] = useState({});
+  const [featuredSection, setFeaturedSection] = useState('home');
 
   useEffect(() => {
-    getFeatured('home')
+    getFeatured(featuredSection)
     .then(data => {
       setArticles(data)
     })
-  }, [])
-
-
+  }, [featuredSection])
 
 
   return (
     <div className="App">
+      <Menu
+        featuredSection={featuredSection}
+        setFeaturedSection={setFeaturedSection}
+      />
       <Articles
         articles={articles}
       />
