bb670ba0771db03871a813ecdfea2da3a00f7e93
Dillon Parker <prkr93@gmail.com>
2022-06-10 11:00:22 -0600

    Reorder display of highlighted article

--- src/Components/HighlightedArticle/HighlightedArticle.js
+++ src/Components/HighlightedArticle/HighlightedArticle.js
@@ -5,21 +5,20 @@ const HighlightedArticle = ({highlightedArticle, setHighlightedArticle}) => {
 
   return (
     <section className='highlighted'>
-      <button onClick={() => setHighlightedArticle({})}>X</button>
+      <button onClick={() => setHighlightedArticle({})}>{`Back to ${highlightedArticle.section.charAt(0).toUpperCase() + highlightedArticle.section.slice(1)}`}</button>
       {highlightedArticle.multimedia && <img className="multimedia" src={highlightedArticle.multimedia[0].url}/>}
+
+      <p className="title">{highlightedArticle.title}</p>
       <p className="abstract">{highlightedArticle.abstract}</p>
       <p className="byline">{highlightedArticle.byline}</p>
-      <p className="created_date">{highlightedArticle.created_date}</p>
-      <p className="item_type">{highlightedArticle.item_type}</p>
       <p className="kicker">{highlightedArticle.kicker}</p>
-      <p className="published_date">{highlightedArticle.published_date}</p>
       <p className="section">{highlightedArticle.section}</p>
-      <p className="short_url">{highlightedArticle.short_url}</p>
       <p className="subsection">{highlightedArticle.subsection}</p>
-      <p className="title">{highlightedArticle.title}</p>
-      <p className="updated_date">{highlightedArticle.updated_date}</p>
-      <p className="uri">{highlightedArticle.uri}</p>
-      <p className="url">{highlightedArticle.url}</p>
+      <p className="item_type">{highlightedArticle.item_type}</p>
+      <p className="created_date">Created: {highlightedArticle.created_date}</p>
+      <p className="published_date">Published: {highlightedArticle.published_date}</p>
+      <p className="updated_date">Updated: {highlightedArticle.updated_date}</p>
+      <a href={highlightedArticle.url} label='See Full Article'>See Full Article</a>
     </section>
   )
 }
